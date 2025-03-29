900ed50914ca81aa768f6fb7138b28b90686a3a4
Gabe Harvey <harvey7@gmail.com>
2025-03-29 00:34:54 -0500

    Video Size Update in AskElla.jsx

--- src/components/AskElla.jsx
+++ src/components/AskElla.jsx
@@ -1,7 +1,7 @@
 import { Box, VStack, Heading, Flex } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 
-const MotionBox = motion.create(Box);
+const MotionBox = motion(Box);
 
 function AskElla() {
   return (
@@ -19,15 +19,16 @@ function AskElla() {
           borderWidth="4px"
           borderColor="#D2042D"
           boxShadow="0 0 15px rgba(0, 0, 0, 0.7)"
-          p={4}
+          p={0} 
           display="flex"
           alignItems="center"
           justifyContent="center"
+          overflow="hidden" 
           initial={{ scale: 0.9, opacity: 0.5 }}
           animate={{ scale: 1, opacity: 1 }}
           transition={{ duration: 2, ease: 'easeOut' }}
         >
-          <video width="100%" height="100%" controls>
+          <video width="100%" height="100%" style={{ objectFit: "cover" }} controls>
             <source src="ella-vid-17.mov" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
